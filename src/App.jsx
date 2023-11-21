9bfcb9681ba5f1cc9256e9bf88da908205f71dfa
Tom Serenander <tom@serenander.se>
2023-11-21 09:22:41 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Avatar from './Avatar'
+
+function App() {
+	return (
+		<>
+			<Avatar src="./images/bob.jpg" alt="Bob Ziroll" />
+			<br />
+			<Avatar>BZ</Avatar>
+			<br />
+			<Avatar />
+		</>
+	)
+}
+
+export default App
